4e9c88a87cce107a7d982a34c0e16f9f75b38dd9
Jose Luis <alphawebdevop@gmail.com>
2023-02-07 13:03:07 +0100

    1.0v Version con añadidos

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/icono-pagina.png">
     <title>Programa reciclaje</title>
     <link rel="stylesheet" href="style/index.css">
 </head>
